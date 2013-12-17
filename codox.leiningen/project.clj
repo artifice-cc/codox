d7debe7cfc19dd4b30a673c7b1d772ef9545fd84
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-12-17 09:12:32 -0500

    Added my namespace and version.

--- codox.leiningen/project.clj
+++ codox.leiningen/project.clj
@@ -1,6 +1,6 @@
-(defproject codox/codox.leiningen "0.6.6"
+(defproject cc.artifice.codox/codox.leiningen "0.7.0-SNAPSHOT"
   :description "Codox Leiningen plugin"
-  :url "https://github.com/weavejester/codox"
+  :url "https://github.com/artifice-cc/codox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject codox "0.6.6"
+(defproject cc.artifice/codox "0.7.0-SNAPSHOT"
   :description "Alias for the codox/codox.leiningen plugin"
-  :url "https://github.com/weavejester/codox"
+  :url "https://github.com/artifice-cc/codox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[codox/codox.leiningen "0.6.6"]]
+  :dependencies [[cc.artifice.codox/codox.leiningen "0.7.0-SNAPSHOT"]]
   :eval-in-leiningen true)
